653411d7a1648ed2c2d4a3eef190ec105cc34274
nelsoneo <nelsoneo@gmail.com>
2022-03-03 23:46:16 -0300

    exemple

---
+++ exemple.js
@@ -0,0 +1,39 @@
+/* VAR */
+// var pi = 3.141592;
+// console.log(pi);
+
+// var pi = 3;  //reatribution
+// console.log(pi);
+
+// if (true){
+//     var pi = 3.141592;
+// }
+
+// console.log(pi);
+
+/* LET */
+
+// let pi = 3.141592;
+// console.log(pi);
+
+// if (true){
+//     let pi = 3.141592;
+// }
+// console.log(pi);
+
+
+/* CONST */
+
+// const pi = 3.141592;
+// console.log(pi);
+
+// if (true){
+//     const pi = 3.141592;
+// }
+
+// console.log(pi);
+
+/*
+
+A declaração de variavel sempre deve começar com [a-zA-Z_$] seguido por [a-zA-Z0-9_$]
+*/
