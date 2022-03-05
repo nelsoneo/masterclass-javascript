25068b7a88a0f010d8f23d97f8ccaffc26fa0ec6
nelsoneo <nelsoneo@gmail.com>
2022-03-05 12:46:20 -0300

    number

---
+++ number.js
@@ -0,0 +1,12 @@
+new Number(10);
+new Number(9.9);
+new Number(0XFF);
+new Number(0b10);
+new Number(0o10);
+
+(123.4).toExponential(10)  //Evela a la n10 
+
+(123.4).toFixed(10)   //completa com 0 na ezquerda
+(10).toFixed(2)
+
+(123.4).toPrecision(10)  //retorna o tamanho o numero + a parte decimal desse total exacto
