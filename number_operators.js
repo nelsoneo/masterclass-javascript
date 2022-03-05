addc33e5d6b38b702a6c8dea503806a5ad633f72
nelsoneo <nelsoneo@gmail.com>
2022-03-05 13:29:24 -0300

    operators

--- number_operators.js
+++ number_operators.js
@@ -1,1 +1,46 @@
+//operadores aritmeticos +, -, *, / e %
+
 2 + 2;
+8 - 3;
+4 * 5;
+9 / 3;
+7 % 2;
+
+//Operadores de atribução  +=, -=, *=, /= e %=
+
+let result = 10;
+result += 2;
+result -= 5;
+result *= 8;
+result /= 2;
+result %= 6;
+
+//operadores de incremento e decrement ++ e --
+
+let res = 10;
+
+res++;  //post increment
+++res; //pre increment
+
+res--;
+--res;
+
+//operators binary 
+// | Or
+// & AND
+// ^ SHORT or Inclusived
+// ~ NOT
+// <<  deslocate left
+// >>  deslocate right
+// >>> deslocate right with change sinal 
+
+4 | 3;
+(4).toString(2)
+(4).toString(2).padStart(32,0)
+3 & 1;
+5 ^ 2;
+~2;
+4 << 2;
+128 >> 1;
+-2 >>> 1;
+(-2 >>> 0).toString(2).padStart(32,0)
