bd850e68c0929c524a50935c861e2fe56044b33e
nelsoneo <nelsoneo@gmail.com>
2022-03-06 21:06:50 -0300

    symbol

---
+++ symbol_1.js
@@ -0,0 +1,21 @@
+Symbol("a");
+Symbol("b");
+Symbol("c");
+
+/*
+
+O Symbol ele é utilizado em operações realizadas pelo interpretador, como no comando for/of ou em operações da String API como match, split, replace e muitas outras
+
+*/
+
+Symbol.hasInstance
+Symbol.isConcatSpreadable
+Symbol.iterator
+Symbol.match
+Symbol.replace
+Symbol.search
+Symbol.split
+Symbol.species
+Symbol.toPrimitive
+Symbol.toStringTag
+Symbol.unscopables
