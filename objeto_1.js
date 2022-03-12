307e4bf4a302572a08e2dbd2c786a94bdf0e675e
nelsoneo <nelsoneo@gmail.com>
2022-03-11 22:08:55 -0300

    object]

---
+++ objeto_1.js
@@ -0,0 +1,10 @@
+/*
+Objeto: é umaa coleção dinâmica de propriedades definidas por chaves, que podem ser do tipo String ou Symbol, e valores que podem ser de qualquer tipo de dado.
+
+É possível criar objetos de várias formas: pelas notação literal, por meio de uma função construtura ou do nétodo create da Object API
+
+*/
+
+{}
+new Object();
+Object.create(null);
