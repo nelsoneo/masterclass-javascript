e11a1b3d17fc7e4064501a0dfb176b80667ce00f
nelsoneo <nelsoneo@gmail.com>
2022-05-31 23:53:36 -0300

    exercies6

--- exercies6.js
+++ exercies6.js
@@ -61,77 +61,109 @@ Array.prototype.forEach
 */
 
 const DatabaseError = function (statement, message) {
-    this.statement = statement;
-    this.message = message;
-  };
-  
-  const database = {
-    tables: {},
-    createTable(statement) {
-      const regexp = /create table ([a-z]+) \((.+)\)/;
-  
-      let result = statement.match(regexp);
+  this.statement = statement;
+  this.message = message;
+};
+
+const database = {
+  tables: {},
+  createTable(statement) {
+    const regexp = /create table ([a-z]+) \((.+)\)/;
+
+    let result = statement.match(regexp);
     //   const tableName = result[1];
-      let [,tableName, columns] = result;
-  
-      this.tables[tableName] = {
-        columns: {},
-        data: [],
-      };
-  
+    let [, tableName, columns] = result;
+
+    this.tables[tableName] = {
+      columns: {},
+      data: [],
+    };
+
     //   let columns = result[2];
-      columns = columns.split(", ");
-  
-      for (let column of columns) {
-        column = column.split(" ");
-        // const name = column[0];
-        // const type = column[1];
-        let [name, type] = column;
-        this.tables[tableName].columns[name] = type;
-      }
-    },
-    insert(statement){
-       const regexp = /insert into ([a-z]+) \((.+)\) values \((.+)\)/;
-       const parsedStatement = statement.match(regexp);
-       let [,tableName, columns, values] = parsedStatement;
-       columns = columns.split(', ');
-       values = values.split(', ');
-       let row = {}
-       for(let i= 0; i < columns.length; i++) {
-           const column = columns[i];
-           const value = values[i];
-           row[column] = value;
-       }
-       this.tables[tableName].data.push(row);
-    },
-    execute(statement) {
-      if (statement.startsWith("create table")) {
-        return this.createTable(statement);
-      }
+    columns = columns.split(", ");
 
-      if(statement.startsWith("insert")){
-          return this.insert(statement);
-      }
-      const message = `Syntax error: "${statement}"`;
-      throw new DatabaseError(statement, message);
-    },
-  };
+    for (let column of columns) {
+      column = column.split(" ");
+      // const name = column[0];
+      // const type = column[1];
+      let [name, type] = column;
+      this.tables[tableName].columns[name] = type;
+    }
+  },
+  insert(statement) {
+    const regexp = /insert into ([a-z]+) \((.+)\) values \((.+)\)/;
+    const parsedStatement = statement.match(regexp);
+    let [, tableName, columns, values] = parsedStatement;
+    columns = columns.split(", ");
+    values = values.split(", ");
+    let row = {};
+    for (let i = 0; i < columns.length; i++) {
+      const column = columns[i];
+      const value = values[i];
+      row[column] = value;
+    }
+    this.tables[tableName].data.push(row);
+  },
+  select(statement) {
+    const regexp = /select (.+) from ([a-z]+)(?: where (.+))?/
+    const parsedStatement = statement.match(regexp);
+    let [, columns, tableName, whereClause] = parsedStatement
+    columns = columns.split(', ')
+    let rows = this.tables[tableName].data
 
+    if(whereClause){
+      let [columnWhere, valueWhere] = whereClause.split(' = ')
+      
+      rows = rows.filter(function (row) { return row[columnWhere] === valueWhere})
+    }
+    
+    rows = rows.map(function(row){
+      let selectRow = {}
+      columns.forEach(function(column){
+        selectRow[column] = row[column]
+      })
+      return selectRow;
+    })
+    return rows;
+  },
+  execute(statement) {
+    if (statement.startsWith("create table")) {
+      return this.createTable(statement);
+    }
+
+    if (statement.startsWith("insert")) {
+      return this.insert(statement);
+    }
+
+    if (statement.startsWith("select")) {
+      return this.select(statement);
+    }
+    const message = `Syntax error: "${statement}"`;
+    throw new DatabaseError(statement, message);
+  },
+};
+
+database.execute(
+  "create table author (id number, name string, age number, city string, state string, country string)"
+);
+
+try {
   database.execute(
     "create table author (id number, name string, age number, city string, state string, country string)"
   );
-  
-  try {
-    
-    database.execute("create table author (id number, name string, age number, city string, state string, country string)");
-    database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)");
-    database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)");
-    database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)");
-    database.execute("select name, age from author");
-    database.execute("select name, age from author where id = 1");
+  database.execute(
+    "insert into author (id, name, age) values (1, Douglas Crockford, 62)"
+  );
+  database.execute(
+    "insert into author (id, name, age) values (2, Linus Torvalds, 47)"
+  );
+  database.execute(
+    "insert into author (id, name, age) values (3, Martin Fowler, 54)"
+  );
+  database.execute("select name, age from author");
+  database.execute("select name, age from author where id = 1");
 
-    console.log(JSON.stringify(database, undefined, " "));
-  } catch (error) {
-      console.log(error.message);
-  }
-  
+  console.log(JSON.stringify(database, undefined, " "));
+} catch (error) {
+  console.log(error.message);
+}
