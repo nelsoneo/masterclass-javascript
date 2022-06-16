dd15ed951ed6c820499af83379b475b4daca342d
nelsoneo <nelsoneo@gmail.com>
2022-06-16 20:19:21 -0300

    weakmap

---
+++ weakmap-get.js
@@ -0,0 +1,17 @@
+/*
+    WeakMap é um objeto, similar ao Map, que permite qpenas chaves do tipo Object e mantém as referências de forma fraca, sendo volátil e não iterável.
+
+    set: Adiciona um par de chave e valor.
+    has: Retorna true se a chave existir.
+    get: Retorna o valor de uma determinada chave.
+    delete: Remove um par de chave e valor.
+*/
+
+const wm1 = new WeakMap();
+const obj1 = {};
+const obj2 = {};
+wm1.set(obj1, 'obj1');
+wm1.set(obj2, 'obj2');
+console.log(wm1.get(obj1));
+console.log(wm1.get(obj2));
+console.log(wm1);
