0f1175e05957de10252650580d10a8b65374fc10
nelsoneo <nelsoneo@gmail.com>
2022-06-16 22:58:32 -0300

    weakset

---
+++ set/weakSet-has.js
@@ -0,0 +1,15 @@
+/**
+ *  WeakSet é um objeto, similar ao Set, que permite apenas valores do tipo object e mantém as referências de forma fraca, sendo volátil e não iteravel.
+ * 
+ * add: Adiciona um elemento.
+ * has: Retorna true se o elemento existir.
+ * delete: Remove um elemento.
+ */
+
+const ws1 = new WeakSet();
+const obj1 = {}
+const obj2 = {}
+ws1.add(obj1)
+ws1.add(obj2)
+console.log(ws1.has(obj1));
+console.log(ws1.has(obj2));
---
+++ set/weakSet.js
@@ -0,0 +1,10 @@
+/**
+ *  WeakSet é um objeto, similar ao Set, que permite apenas valores do tipo object e mantém as referências de forma fraca, sendo volátil e não iteravel.
+ * 
+ * add: Adiciona um elemento.
+ * has: Retorna true se o elemento existir.
+ * delete: Remove um elemento.
+ */
+
+const ws1 = new WeakSet();
+console.log(ws1);
